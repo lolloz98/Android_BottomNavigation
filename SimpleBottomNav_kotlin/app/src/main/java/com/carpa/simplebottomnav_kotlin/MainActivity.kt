100f816412c3662828d166d35347d68d20693493
lolloz98 <lorenzocarpaneto@yahoo.it>
2020-03-29 01:28:55 +0100

    Kotlin version uploaded.

---
+++ SimpleBottomNav_kotlin/app/src/main/java/com/carpa/simplebottomnav_kotlin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.carpa.simplebottomnav_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        bottom_nav_view.setupWithNavController(navController)
+    }
+}
